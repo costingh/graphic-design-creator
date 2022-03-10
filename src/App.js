2c53523d59fe9074c8fc7647541493b35896a9d4
Costin <gheorghe.costin2001@yahoo.com>
2022-03-10 04:44:05 -0800

    added react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,74 +1,22 @@
-import './App.css';
-import GoogleLogin from 'react-google-login';
-import {useState, useEffect} from 'react'
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Dashboard from "./components/Dashboard";
+import Login from "./components/Login";
 
+import "./App.css";
 
 function App() {
-
-  const [loginData, setLoginData] = useState(
-    localStorage.getItem('loginData')
-      ? JSON.parse(localStorage.getItem('loginData'))
-      : null
-  );
-
-  useEffect(() => {
-    if(loginData) alert('Authenticated') 
-  }, [loginData])
-  
-  const handleFailure = (result) => {
-    alert(result);
-  };
-
-  const handleLogin = async (googleData) => {
-    const res = await fetch('/api/google-login', {
-      method: 'POST',
-      body: JSON.stringify({
-        token: googleData.tokenId,
-      }),
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    });
-
-    const data = await res.json();
-    setLoginData(data);
-    localStorage.setItem('loginData', JSON.stringify(data));
-  };
-
-  const handleLogout = () => {
-    localStorage.removeItem('loginData');
-    setLoginData(null);
-  };
-
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Log in</h1>
-        <div>
-          {/* {loginData ? (
-            <div>
-              <h3>You logged in as {loginData.email}</h3>
-              <button onClick={handleLogout}>Logout</button>
-            </div>
-          ) : (
-            <GoogleLogin
-              clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}
-              buttonText="Log in with Google"
-              onSuccess={handleLogin}
-              onFailure={handleFailure}
-              cookiePolicy={'single_host_origin'}
-            ></GoogleLogin>
-          )} */}
-
-          {!loginData && <GoogleLogin
-              clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}
-              buttonText="Log in with Google"
-              onSuccess={handleLogin}
-              onFailure={handleFailure}
-              cookiePolicy={'single_host_origin'}
-            ></GoogleLogin>}
-        </div>
-      </header>
+      <Router>
+        <Switch>
+          <Route path="/dashboard">
+            <Dashboard />
+          </Route>
+          <Route path="/">
+            <Login />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
